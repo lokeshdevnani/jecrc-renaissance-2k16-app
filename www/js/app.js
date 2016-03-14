de09696fe16c2d74349c86e52e36b1d2e9388404
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-03-14 22:33:26 +0530

    cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,9 @@
-// Ionic ionic-todo App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'ionic-todo' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('ionic-todo', ['ionic', 'LocalStorageModule']);
+var app = angular.module('renApp', ['ionic', 'LocalStorageModule']);
 
 app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
             cordova.plugins.Keyboard.disableScroll(true);
         }
         if (window.StatusBar) {
@@ -60,10 +49,5 @@ app.config(function ($stateProvider, $urlRouterProvider ,localStorageServiceProv
 });
 
 app.controller('main', function ($scope, $ionicModal, localStorageService) {
-    //store the entities name in a variable
-
-    var taskData = 'task';
-
-    $scope.hello = 'world';
 
 });
